a1a2b8c9c42742bf22bb37616507495f28c24e9f
Nikola Posa <posa.nikola@gmail.com>
2016-12-16 19:33:41 +0100

    Deploy script.

---
+++ deploy.php
@@ -0,0 +1,45 @@
+<?php
+
+require 'recipe/common.php';
+
+date_default_timezone_set('UTC');
+
+$config = require __DIR__ . '/deploy/config.php';
+
+serverList($config['servers']);
+
+foreach ($config['parameters'] as $key => $value) {
+    set($key, $value);
+}
+
+set('writable_dirs', [
+    'log',
+    'web/uploads',
+]);
+set('shared_files', [
+    'config/production.yml',
+    'config/development.yml',
+]);
+
+task('phinx:migrate', function () {
+    $output = run('cd {{release_path}} && ./vendor/bin/phinx migrate');
+    writeln('<info>' . $output . '</info>');
+})->desc('Run database migrations');
+
+task('phinx:migrate:rollback', function () {
+    $output = run('cd {{release_path}} && ./vendor/bin/phinx rollback');
+    writeln('<info>' . $output . '</info>');
+})->desc('Rollback database migrations');
+
+task('deploy', [
+    'deploy:prepare',
+    'deploy:release',
+    'deploy:update_code',
+    'deploy:shared',
+    'deploy:vendors',
+    'phinx:migrate',
+    'deploy:symlink',
+    'cleanup',
+])->desc('Deploy project');
+
+after('deploy', 'success');
